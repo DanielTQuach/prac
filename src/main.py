44da94dd4f019e476591cd27b62c585327d63b3f
Daniel Quach <danielq2k15@gmail.com>
2026-05-12 00:02:14 -0700

    add api usage endpoint

--- src/main.py
+++ src/main.py
@@ -13,3 +13,11 @@ def read_item(item_id: int):
 @app.post("/items")
 def create_item(item: dict):
     return {"received": item}
+
+@app.get("api/usage")
+def get_usage():
+    return {
+        "success": 6,
+        "failed": 7,
+        "window": "1h",
+    }
