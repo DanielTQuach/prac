7ea795b65c304b2289fd8f8bcefbd26affcfce4c
Daniel Quach <danielq2k15@gmail.com>
2026-05-27 23:53:27 -0700

    add booking form

--- php/SQLtest.php
+++ php/SQLtest.php
@@ -13,4 +13,35 @@ if ($conn->connect_error) {
 }
 
 echo "Connected succesfully";
+
+$sql = "SELECT" * FROM services";
+$result = $conn->query($sql);
+
+<!DOCTYPE html>
+<html>
+<head><title>Booking Service</titlle></head>
+<body>
+    <h1>Book an Appointment</h1>
+    <form action="book.php" method="POST">
+        <label>Select Service:</label>
+        <select name="serice_id" required>
+            <?php while($row = $result->fetch_assoc()) { ?>
+                <option value="<?php echo $row['id']; ?>">
+                    <?php echo $row['name']; ?> - $<?php echo $row['price']; ?>
+                </option>
+            <?php } ?>
+        </select>
+        <br><br>
+        <label>Date & Time:</label>
+        <input type="datetime-local" name="booking_time" required>
+        <br><br>
+        <input type="text" name="customer_name" placeholder="Your Name" required>
+        <input type="email" name="customer_email" placeholder="Your Email" required>
+        <br><br>
+        <button type="submit">Book Now</button>
+    </form>
+
+    
+</body>
+</html>
 ?>
